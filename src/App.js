3a3b07bc92dde14f14221e6ccb98744942dd8705
Nishantrobo <nishant9843.kumar@gmail.com>
2023-04-09 17:22:29 +0530

    trying somedifferent tecnhiques on creating new branches

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import './App.css';
 
 function App() {
   return (
-    <div><h1>assigment day 3</h1></div>
+    <div><h1>assigment day 3</h1>
+    <h2>learning to uploading new branches on github and trying different techniques .</h2>
+    </div>
   ) 
 }
 export default App;
