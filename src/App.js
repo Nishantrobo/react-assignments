149dc81c4a1b440315f2989296e4d42bc9586154
Nishantrobo <nishant9843.kumar@gmail.com>
2023-04-09 17:51:40 +0530

    challenge 1 of day3 assigment

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import './App.css';
 
 function App() {
   return (
-    <div><h1>assigment day 3</h1>
-    <h2>learning to uploading new branches on github and trying different techniques .</h2>
-    <p> im changing in reactday3 assigment to see changes how commits works in new branch
-    </p>
-    <p> now i will push this in github to see what has cahnged on github</p>
+    <div className='App'>
+      <h1>Register Here....</h1>
+      <input placeholder='mobile...'></input>
+      <br></br>
+      <br></br>
+      <input type={'password'} placeholder='password...'></input>
+      <br></br>
+      <br></br>
+      <button>submit</button>
+
     </div>
-  ) 
+  )
 }
 export default App;
