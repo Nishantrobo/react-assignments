788f4aeb254c7e8078c2c6b31df58d93a5bb7d01
Nishantrobo <nishant9843.kumar@gmail.com>
2023-04-09 17:08:51 +0530

    learning how to create branches via gitbash

--- src/App.js
+++ src/App.js
@@ -1,50 +1,8 @@
 import './App.css';
 
-function App1() {
+function App() {
   return (
-    <div className="App">
-     <h1>i am heading one ....from first component </h1>
-     <h2>i am heading two ....from second component </h2>
-     <h3>i am heading three ....from comonent three</h3>
-     <button>click me </button>
-    </div>
-  );
+    <div><h1>assigment day 3</h1></div>
+  ) 
 }
-
-//first component
- function HeadingOne(){
-  return <h1>i am heading one...from first component</h1>
- }
-
-//second component 
-function HeadingTwo(){
-  return <h2>i am heading two....from second component</h2> 
-}
-
-//third component 
-
-function HeadingThree(){
-  return <h3> i am heading three.....from third component</h3>
-}
-
-//fourth  component
-
-function Button(){
-  return <button>click me</button>
-}
-
-function App(){
-  return(
-    <div>
-      <App1/>
-      <HeadingOne/>
-      <HeadingTwo/>
-      <HeadingThree/>
-      <Button/>
-
-    </div>
-  )
-}
-
-
 export default App;
