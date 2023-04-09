ecbe28e184f59412f7bb89c5f0de628cdbb5c879
Nishantrobo <nishant9843.kumar@gmail.com>
2023-04-09 17:28:36 +0530

    second change to see what has been changed on github

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ function App() {
   return (
     <div><h1>assigment day 3</h1>
     <h2>learning to uploading new branches on github and trying different techniques .</h2>
+    <p> im changing in reactday3 assigment to see changes how commits works in new branch
+    </p>
+    <p> now i will push this in github to see what has cahnged on github</p>
     </div>
   ) 
 }
